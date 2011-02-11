7e3cc151591472fe1f4682bbcf2dc99ffa173a38
Joel Hagen <jchagen@gmail.com>
2011-02-10 19:56:11 -0600

    test

--- Gemfile
+++ Gemfile
@@ -11,5 +11,6 @@ end
 group :test do
   gem 'rspec', '2.3.0'
   gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
 end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,21 @@
 require 'spec_helper'
 
 describe UsersController do
+	def show
+		@user = User.find(params[:id])
+	end
+
+  render_views
 
   describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-    
-		it "should have the right title" do
-      get 'new'
-# JCH (01/13/2011) I can't get this to work
-#      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Sign up")
-    end
-  end
-
+	  it "should be successful" do
+	    get 'new'
+	    response.should be_success
+	  end
+	
+	  it "should have the right title" do
+	    get 'new'
+	    response.should have_selector("title", :content => "Sign up")
+	  end
+	end
 end
